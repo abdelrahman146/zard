6936c21b56f8d6abaaba40fa9746c1b56ced4cc3
Abdel Rahman Hussein <abdelrahman146@outlook.com>
2024-07-25 20:45:48 +0400

    implmented login with magic link and password recovery

--- service/account/pkg/usecase/auth_usecase.go
+++ service/account/pkg/usecase/auth_usecase.go
@@ -3,10 +3,14 @@ package usecase
 import (
 	"encoding/json"
 	"errors"
+	"github.com/abdelrahman146/zard/service/account/pkg/model"
 	"github.com/abdelrahman146/zard/service/account/pkg/repo"
 	"github.com/abdelrahman146/zard/shared"
 	"github.com/abdelrahman146/zard/shared/cache"
 	"github.com/abdelrahman146/zard/shared/config"
+	"github.com/abdelrahman146/zard/shared/pubsub"
+	"github.com/abdelrahman146/zard/shared/pubsub/messages"
+	"github.com/abdelrahman146/zard/shared/rpc"
 	"gorm.io/gorm"
 	"time"
 )
@@ -27,45 +31,46 @@ type UserResponse struct {
 
 type AuthUseCase interface {
 	AuthenticateUserByEmailPassword(email, password string) (token string, user *UserResponse, err error)
+	SendMagicLink(email string, withReset bool) error
+	AuthenticateWithMagicLink(magiclinkToken string) (token string, user *UserResponse, err error)
 	AuthenticateToken(token string) (user *UserResponse, err error)
 	AuthenticateWorkspaceByApiKey(apiKey string) (id string, err error)
 }
 
 var (
 	ErrInvalidEmailOrPassword = errors.New("invalid email or password")
+	ErrSomethingWentWrong     = errors.New("something went wrong")
+	ErrUserNotFound           = errors.New("user not found")
+	ErrInactiveUser           = errors.New("user is inactive")
 	ErrInvalidToken           = errors.New("invalid or expired token")
 	ErrInvalidApiKey          = errors.New("invalid apikey")
 )
 
 type authUseCase struct {
-	userRepo    repo.UserRepo
-	wrkRepo     repo.WorkspaceRepo
-	conf        config.Config
-	cacheClient cache.Cache
+	userRepo     repo.UserRepo
+	wrkRepo      repo.WorkspaceRepo
+	conf         config.Config
+	cacheClient  cache.Cache
+	rpcClient    rpc.RPC
+	pubsubClient pubsub.PubSub
 }
 
-func NewAuthUseCase(userRepo repo.UserRepo, wrkRepo repo.WorkspaceRepo, conf config.Config, cacheClient cache.Cache) AuthUseCase {
+func NewAuthUseCase(userRepo repo.UserRepo, wrkRepo repo.WorkspaceRepo, conf config.Config, cacheClient cache.Cache, rpcClient rpc.RPC, pubsubClient pubsub.PubSub) AuthUseCase {
 	return &authUseCase{
-		userRepo:    userRepo,
-		wrkRepo:     wrkRepo,
-		conf:        conf,
-		cacheClient: cacheClient,
+		userRepo:     userRepo,
+		wrkRepo:      wrkRepo,
+		conf:         conf,
+		cacheClient:  cacheClient,
+		rpcClient:    rpcClient,
+		pubsubClient: pubsubClient,
 	}
 }
 
-func (uc *authUseCase) AuthenticateUserByEmailPassword(email, password string) (token string, user *UserResponse, err error) {
-	userModel, err := uc.userRepo.GetOneByEmail(email)
-	if err != nil {
-		return "", nil, ErrInvalidEmailOrPassword
-	}
-	hashedPassword := shared.Utils.Auth.Encrypt(password, uc.conf.GetString("app.secret"))
-	if userModel.Password != nil && *userModel.Password != hashedPassword {
-		return "", nil, ErrInvalidEmailOrPassword
-	}
-	token = shared.Utils.Auth.CreateToken("ztkn", userModel.ID, uc.conf.GetString("app.secret"))
+func (uc *authUseCase) createUserSession(userModel *model.User) (token string, user *UserResponse, err error) {
 	userJson, _ := json.Marshal(userModel)
+	token = shared.Utils.Auth.CreateToken("ztkn", userModel.ID, uc.conf.GetString("app.secret"))
 	ttl := time.Second * time.Duration(uc.conf.GetInt("app.auth.tokenTTL"))
-	if err = uc.cacheClient.Set([]string{"account", "auth", "user", "tokens", token}, userJson, ttl); err != nil {
+	if err := uc.cacheClient.Set([]string{"account", "auth", "user", "tokens", token}, userJson, ttl); err != nil {
 		return "", nil, err
 	}
 	user = &UserResponse{
@@ -84,6 +89,23 @@ func (uc *authUseCase) AuthenticateUserByEmailPassword(email, password string) (
 	return token, user, nil
 }
 
+func (uc *authUseCase) AuthenticateUserByEmailPassword(email, password string) (token string, user *UserResponse, err error) {
+	userModel, err := uc.userRepo.GetOneByEmail(email)
+	if err != nil {
+		return "", nil, ErrUserNotFound
+	}
+	if userModel.Password == nil {
+		return "", nil, ErrInvalidEmailOrPassword
+	}
+	if userModel.Active == false {
+		return "", nil, ErrInactiveUser
+	}
+	if ok := shared.Utils.Auth.Compare(*userModel.Password, password, uc.conf.GetString("app.secret")); !ok {
+		return "", nil, ErrInvalidEmailOrPassword
+	}
+	return uc.createUserSession(userModel)
+}
+
 func (uc *authUseCase) AuthenticateToken(token string) (user *UserResponse, err error) {
 	userJson, err := uc.cacheClient.Get([]string{"account", "auth", "user", "tokens", token})
 	if err != nil {
@@ -93,6 +115,39 @@ func (uc *authUseCase) AuthenticateToken(token string) (user *UserResponse, err 
 		return nil, err
 	}
 	return user, nil
+}
+
+func (uc *authUseCase) SendMagicLink(email string, withReset bool) error {
+	user, err := uc.userRepo.GetOneByEmail(email)
+	if err != nil {
+		return err
+	}
+	token := shared.Utils.Auth.CreateToken("zml", user.ID, uc.conf.GetString("app.secret"))
+	ttl := time.Second * time.Duration(uc.conf.GetInt("app.auth.magicLinkTTL"))
+	if err = uc.cacheClient.Set([]string{"account", "auth", "user", "magiclinks", token}, []byte(user.ID), ttl); err != nil {
+		return err
+	}
+	msg := messages.MagicLinkMessage{
+		Email:     email,
+		Name:      user.Name,
+		Token:     token,
+		Timestamp: time.Now(),
+		WithReset: withReset,
+	}
+
+	return uc.pubsubClient.Publish(&msg)
+}
+
+func (uc *authUseCase) AuthenticateWithMagicLink(magiclinkToken string) (token string, user *UserResponse, err error) {
+	userId, err := uc.cacheClient.Get([]string{"account", "auth", "user", "magiclinks", magiclinkToken})
+	if err != nil {
+		return "", nil, ErrInvalidToken
+	}
+	userModel, err := uc.userRepo.GetOneByID(string(userId))
+	if err != nil {
+		return "", nil, err
+	}
+	return uc.createUserSession(userModel)
 }
 
 func (uc *authUseCase) AuthenticateWorkspaceByApiKey(apiKey string) (id string, err error) {
